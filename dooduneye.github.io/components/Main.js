c30b81510eec2844028e10f6625d1274eff0db2a
David Oduenye <davidoduneye1@gmail.com>
2022-10-26 23:24:25 -0400

    nextjs

--- dooduneye.github.io/components/Main.js
+++ dooduneye.github.io/components/Main.js
@@ -3,6 +3,8 @@ import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 import { motion } from 'framer-motion';
 
+import tw from 'tailwind-styled-components';
+
 const variants = {
     hidden: { opacity: 0, y: 100 },
     animate: { opacity: 1, y: 0, transition: { duration: 1, delay: 0.5, ease: [0.6, 0.05, -0.01, 0.9] } },
@@ -11,36 +13,89 @@ const variants = {
 const gradientVariants = {
     hidden: { color: '#000000' },
     animate: { color: 'transparent', 
-        background: 'linear-gradient(90deg, #F43B47 0%, #453A94 100%)',
+        background: 'linear-gradient(90deg, #B4A0E5 0%, #4ECDC4 100%)',
         WebkitBackgroundClip: 'text',
         WebkitTextFillColor: 'transparent',
         transition: { duration: 4, delay: 0.5, ease: [0.6, 0.05, -0.03, 0.9] } },
 }
 
+const FlexColContainer = tw.section`
+    flex
+    flex-col
+`
+
+const FlexRowContainer = tw.section`
+    flex
+    flex-row
+`
+
+const UnderlineTransition = tw.span`
+    hover:underline
+    hover:decoration-sky-600 
+    hover:underline-offset-8 
+    hover:decoration-3 
+    hover:transition
+    hover:duration-300 
+    hover:ease-in-out 
+    hover:delay-150 
+    hover:translatex-1
+`
+
+interface GradientTransitionProps {
+    $gradient: string;
+
+}
+
+interface ButtonProps {
+    $primary: boolean
+ }
+
+
+const GradientP = tw.p<{from: string, to: string}>`
+    font-sans
+    font-bold 
+    text-transparent
+    text-3xl
+    bg-clip-text
+    bg-gradient-to-r
+    ${props => props.from}
+    ${props => props.to}
+`
+
+const motionGradientP = motion.custom(GradientP);
+
+
+
 
 export default function Main() {
     return (
-        <section className="flex flex-row p-12">
-            <div className="flex flex-col">
-                <div className="flex flex-row gap-20">
+        <FlexRowContainer className="p-12">
+            <FlexColContainer>
+                <FlexRowContainer className="gap-20">
                     <div>
-                        <h1 className="font-sans font-bold text-8xl pb-3 text-slate-100">
+                        <h1 className="
+                                    font-sans 
+                                    font-bold 
+                                    text-8xl 
+                                    pb-3
+                                    text-slate-100">
                            <motion.span variants={variants} initial="hidden" animate="animate" className="drop-shadow-lg">
                             <motion.span initial='initial' animate='animate' variants={gradientVariants}>
-                                Hi, I'm David <motion.a variants={variants} initial="hidden" animate="animate" href="mailto:oduneye.d@northeastern.edu" className="text-[#453A94] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 drop-shadow-md"><FontAwesomeIcon icon={faPaperPlane} /></motion.a>
+                                Hi, I'm David <motion.a variants={variants} initial="hidden" animate="animate" href="mailto:oduneye.d@northeastern.edu" className="text-[#4ECDC4] transition ease-in-out delay-150 hover:-translate-y-1 hover:scale-110 duration-300 drop-shadow-md"><FontAwesomeIcon icon={faPaperPlane} /></motion.a>
                             </motion.span>
                            </motion.span>
                         </h1>
 
-                        <motion.p variants={variants} initial="hidden" animate="animate" className="font-sans font-bold text-transparent text-3xl bg-clip-text bg-gradient-to-r from-pink-500 to-[#453A94]">
+                        <motionGradientP variants={variants} initial="hidden" animate="animate" from="from-slate-100" to="to-slate-200" className="drop-shadow-lg">
                             Passionate about building 
                             <br /> 
-                            software that is both <span className="hover:underline hover:decoration-sky-600 hover:underline-offset-8 hover:decoration-3 hover:transition hover:duration-300 hover:ease-in-out hover:delay-150 hover:translatex-1">beautiful</span> and <span className="hover:underline hover:decoration-sky-600 hover:underline-offset-8 hover:decoration-3 hover:transition hover:duration-300 hover:ease-in-out hover:delay-150 ">functional</span>.
-                        </motion.p>
+                            software that is both <UnderlineTransition>beautiful</UnderlineTransition> and <UnderlineTransition>functional</UnderlineTransition>.
+                        </motionGradientP>
                     </div>
 
-                </div>
-            </div>
-        </section>
+            </FlexRowContainer>
+        </FlexColContainer>
+    </FlexRowContainer>
+    
     );
 }
--- dooduneye.github.io/next.config.js
+++ dooduneye.github.io/next.config.js
@@ -1,18 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-import nextMDX from '@next/mdx'
+// import nextMDX from '@next/mdx'
 
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    providerImportSource: '@mdx-js/react',
-  },
-})
+// const withMDX = nextMDX({
+//   extension: /\.mdx?$/,
+//   options: {
+//     providerImportSource: '@mdx-js/react',
+//   },
+// })
 
-export default withMDX({
-  // Support MDX files as pages:
-  pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
-})
+// export default withMDX({
+//   // Support MDX files as pages:
+//   pageExtensions: ['md', 'mdx', 'tsx', 'ts', 'jsx', 'js'],
+// })
 
 const nextConfig = {
   reactStrictMode: true,
