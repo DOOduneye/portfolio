96005c1488896cf77429c398cf3825bc626463f7
David Oduenye <davidoduneye1@gmail.com>
2022-11-03 23:50:42 -0400

    fixed: projects and posts

--- components/Main/Title.js
+++ components/Main/Title.js
@@ -3,17 +3,14 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faPaperPlane } from '@fortawesome/free-solid-svg-icons';
 
 import tw from 'tailwind-styled-components';
+// import tw, { css, styled, theme } from 'twin.macro';
 
 const HeaderText = tw.h1`
-    text-slate-100
-    lg:text-9xl
-    text-7xl
-    font-sans 
-    font-bold 
-    pb-3
-`;
+    text-slate-100 lg:text-9xl text-7xl font-sans font-bold pb-3`
+;
 
 const Icon = tw.a`
+    tw
     md:inline-block
     hidden
     text-[#B4A0E5]
@@ -22,19 +19,15 @@ const Icon = tw.a`
     pl-2
 `;
 
-const Gradient = tw.div`
-    bg-clip-text
-    text-transparent
-    bg-gradient-to-r
-    from-[#4ECDC4]
-    to-[#B4A0E5]
-    max-h-fit
-`;
+const Gradient = tw.span`
+    bg-clip-text text-transparent bg-gradient-to-r from-[#4ECDC4] to-[#B4A0E5]`
+;
+
 
 const Title = () => {
     return (
         <HeaderText>
-            <span className="drop-shadow-lg">
+            <span className="drop-shadow-lg pb-20">
                 <Gradient>
                     David Oduneye
 
