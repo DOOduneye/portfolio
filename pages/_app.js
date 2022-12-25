fd20ab3905a98fc56407d366b4d40a35c5ae2ef2
David Oduenye <davidoduneye1@gmail.com>
2022-12-25 12:14:22 -0500

    add: analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,26 @@
 import '../styles/globals.scss';
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
 
-const MyApp = ({ Component, pageProps, router }) => <Component {...pageProps} key={router.route} />;
+const MyApp = ({ Component, pageProps, router }) => {
+
+    const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+    };
+
+    const [loading, setLoading] = useState(true);
+    
+    useEffect(() => {
+        const app = initializeApp(firebaseConfig);
+        const db = getFirestore(app);
+        setLoading(false);
+    }, []);
+
+    if (loading) {
+        return <div>Loading...</div>;
+    }
+
+    return <Component {...pageProps} />;
+}
 
 export default MyApp;
