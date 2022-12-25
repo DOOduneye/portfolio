c2c198c23703a01388127af4a00f89786c0cecdd
David Oduenye <davidoduneye1@gmail.com>
2022-12-25 12:24:26 -0500

    fix: firebase config

--- pages/_app.js
+++ pages/_app.js
@@ -3,27 +3,18 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const MyApp = ({ Component, pageProps, router }) => {
-    const [loading, setLoading] = useState(true);
-    
-    useEffect(() => {
-        const firebaseConfig = {
-            apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-            authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-            projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-            storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-            appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-            measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
-        };
+    const firebaseConfig = {
+        apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+        authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+        projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+        storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+        messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+        appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+        measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+    };
 
-        const app = initializeApp(firebaseConfig);
-        const db = getFirestore(app);
-        setLoading(false);
-    }, [firebaseConfig]);
-
-    if (loading) {
-        return <div>Loading...</div>;
-    }
+    const app = initializeApp(firebaseConfig);
+    const db = getFirestore(app);
 
     return <Component {...pageProps} />;
 }
