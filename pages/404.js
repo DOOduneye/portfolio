66e140efd52102a20c2d2450985a87c035dd8e79
David Oduenye <davidoduneye1@gmail.com>
2022-12-25 05:07:23 -0500

    fix: /uses dependency issue

--- pages/404.js
+++ pages/404.js
@@ -1,99 +1,33 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faArrowLeft, faFrownOpen } from '@fortawesome/free-solid-svg-icons';
 
-import { motion } from 'framer-motion';
-import variants from '../styles/motion';
-
-import tw from 'tailwind-styled-components';
-
-const Container = tw.main`
-    min-h-full 
-    px-4 
-    py-16 
-    sm:px-6 
-    sm:py-24 
-    md:grid 
-    md:place-items-center 
-    lg:px-8
-`;
-
-const MotionContainer = motion(Container);
-
-const SubContainer = tw.div`
-    max-w-max
-    mx-auto
-`;
-
-const NotFoundText = tw.p`
-    text-4xl 
-    font-bold 
-    tracking-tight 
-    sm:text-5xl 
-    bg-clip-text 
-    text-transparent
-    bg-gradient-to-r 
-    from-pink-500 
-    to-red-300
-`;
-
-const NotFoundSubText = tw.p`
-    mt-1
-    text-base
-    text-gray-500   
-`;
-
-const Button = tw.a`
-    bg-gradient-to-r 
-    from-pink-500 
-    to-red-300 
-    hover:bg-gradient-to-r 
-    transition 
-    ease-in-out 
-    delay-150 
-    hover:-translate-y-1 
-    hover:scale-100 
-    duration-300 
-    inline-flex 
-    items-center 
-    justify-center 
-    px-5 
-    py-3 
-    text-base 
-    font-medium 
-    rounded-full 
-    text-slate-100 
-    hover:text-white 
-    drop-shadow-lg 
-    shadow-lg
-`;
+import Link from 'next/link';
 
 export default function NotFound() {
     return (
-        <MotionContainer initial="initial" animate="animate" exit="exit" variants={variants}>
-            <SubContainer>
-                <main className="sm:flex">
-                    <NotFoundText>{'404'}</NotFoundText>
-                    <div className="sm:ml-6">
-                        <div className="sm:border-l sm:border-gray-200 sm:pl-6">
-                            <h1 className="text-4xl font-bold tracking-tight text-slate-200 sm:text-5xl">
-                                <FontAwesomeIcon icon={faFrownOpen} /> {'Page not found'}
-                            </h1>
-                            <NotFoundSubText>
-                                {'The page you\'re looking for doesn\'t exist. It may have been moved or deleted.'}
-                            </NotFoundSubText>
-                        </div>
-                        <div className="flex mt-10 space-x-3 sm:border-l sm:border-transparent sm:pl-6">
-                            <Button href="/">
-                                <FontAwesomeIcon
-                                    icon={faArrowLeft}
-                                    className="mr-2 transition duration-300 ease-in-out translate-x-0 delay hover:-translate-x-1"
-                                />
-                                {'Lets go home'}
-                            </Button>
-                        </div>
-                    </div>
-                </main>
-            </SubContainer>
-        </MotionContainer>
+        <main className="flex flex-col items-center justify-center w-screen mt-5">
+            <div className="flex flex-col justify-center gap-5 px-5 py-8 mx-auto font-sans text-lg font-normal leading-normal text-left break-words align-middle max-w-prose text-slategrey-50 subpixel-antialiase text-slate-100">
+                <h1 className="pb-3 font-sans text-5xl font-bold max-w-prose text-slate-100 lg:text-6xl">
+                    <span className="drop-shadow-lg">
+                        404
+                    </span>
+                </h1>
+
+                <p className="pb-5 font-sans text-lg font-normal leading-normal text-left break-words align-middle max-w-prose text-slategrey-50 subpixel-antialiase text-slate-100">
+                    Page not found.
+                </p>
+
+                <hr className="w-10 pb-5 mx-auto border-t border-slate-100/20" />
+
+                <p className="pb-5 font-sans text-lg font-normal leading-normal text-left break-words align-middle max-w-prose text-slategrey-50 subpixel-antialiase text-slate-100">
+                    <Link href="/" className="text-slate-100 hover:text-slate-200">
+                        <FontAwesomeIcon icon={faArrowLeft} className="mr-2" />
+                        Go back home
+                    </Link>
+                </p>
+            </div>
+        </main>
     );
 }
+
+export { NotFound}
