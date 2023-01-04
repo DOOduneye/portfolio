881af8afacb1252048853b3d8abe2124d9fe130c
David Oduenye <davidoduneye1@gmail.com>
2023-01-04 13:01:26 -0500

    fixed: card component coloring

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -15,7 +15,7 @@ const PostPage = ({ frontmatter: { title, date }, mdxSource }) => {
 
     return (
         <>
-            <button className="relative top-0 left-0 z-50 flex items-center justify-center w-12 h-12 p-2 m-5 text-xl font-bold rounded-full cursor-pointer text-slate-900 dark:text-slate-100 focus:outline-none focus:ring-2 focus:ring-slate-900 dark:focus:ring-slate-100" onClick={() => window.history.back()}>
+            <button className="relative top-0 left-0 z-50 flex items-center justify-center w-12 h-12 p-2 m-5 text-xl font-bold rounded-full cursor-pointer text-slate-900 dark:text-slate-100" onClick={() => window.history.back()}>
                 <svg xmlns="http://www.w3.org/2000/svg" className="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
                     <path strokeLinecap="round" strokeLinejoin="round" strokeWidth={2} d="M10 19l-7-7m0 0l7-7m-7 7h18" />
                 </svg>
