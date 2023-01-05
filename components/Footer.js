cc9d94a9306194465cc634649259d52542f258c3
David Oduenye <davidoduneye1@gmail.com>
2023-01-05 09:54:51 -0500

    update: footer

--- components/Footer.js
+++ components/Footer.js
@@ -4,9 +4,7 @@ const Footer = () => {
     return (
         <>
             <section className="flex flex-row items-center justify-between w-full h-24 px-2 mt-10 border-t sm:px-5 white dark:text-slate-200 text-slate-900 border-slate-400/30">
-                <p className="text-sm">
-                    {`© ${new Date().getFullYear()} David Oduneye`} 
-                </p>
+                {`© ${new Date().getFullYear()} David Oduneye`} 
                 <span className="flex flex-row gap-5">
                     <Link href="/assets/text/Resume.pdf" className="hover:underline underline-offset-4">
                         <> Resume </>
@@ -14,13 +12,9 @@ const Footer = () => {
                     <a href="/assets/text/README.html" className="hover:underline underline-offset-4">
                         <> README.md </>
                     </a>
-                </span>
-            </section>
-            <section className="flex flex-row items-center justify-center px-5 px-2 dark:text-slate-200 text-slate-900 border-slate-400/30">
-                <p className="text-sm">
-                    {`Made with Next.js, TailwindCSS, and Radix UI`} &#8211; {`Last updated: ${new Date().toLocaleDateString()}`}
 
-                </p>
+                    {`Last updated: ${new Date().toLocaleDateString()}`}
+                </span>
             </section>
             
         </>
