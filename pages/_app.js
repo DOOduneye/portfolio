06431ffcfdc0cf361c400d7c75ea6ecfb0b4b42a
David Oduenye <davidoduneye1@gmail.com>
2023-05-06 12:12:31 -0700

    remove: firebase

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,8 @@
 import "../styles/globals.scss";
 
-import { initializeApp } from 'firebase/app';
-import { firebaseConfig } from '@/utils/firebase';
 import { ThemeProvider } from "@/providers/ThemeProvider";
 
 const MyApp = ({ Component, pageProps }) => {
-
-	const app = initializeApp(firebaseConfig);
 
 	return (
 		<ThemeProvider>
