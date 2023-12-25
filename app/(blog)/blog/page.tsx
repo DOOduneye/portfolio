90ad64017fa90c9925398b1a52bbc27836f00c4e
David Oduneye <davidoduneye1@gmail.com>
2023-12-25 12:15:04 -0500

    refactor: moved components into their own folder

--- app/(blog)/blog/page.tsx
+++ app/(blog)/blog/page.tsx
@@ -1,5 +1,7 @@
 import Link from "next/link";
 
+import { PostCard } from "../_components/post-card";
+
 const Blog = () => {
     return (
         <main className="flex flex-col gap-4">
@@ -19,20 +21,3 @@ const Blog = () => {
 }
 
 export default Blog;
-
-export const PostCard = ({ title, description, date, slug, timeToRead }: { title: string, description: string, date: string, slug: string, timeToRead?: number }) => {
-    return (
-        <Link href={`/blog/${slug}`} className='p-5 transition-all duration-300 ease-in-out transform border-2 border-transparent border-gray-200 shadow-sm dark:border-gray-900 dark:hover:border-gray-300 rounded-xl hover:scale-105 hover:border-gray-500'>
-            <div className='flex flex-row justify-between gap-x-4'>
-                <h3 className='text-xl font-bold'>
-                    {title}
-                </h3>
-                <p className='text-sm text-gray-500'>{date}</p>
-            </div>
-            <div className='flex flex-row justify-between gap-x-4'>
-                <p className='text-sm text-gray-500'>{description}</p>
-                <p className='text-sm text-gray-500'>{timeToRead} min read</p>
-            </div>
-        </Link >
-    );
-}
