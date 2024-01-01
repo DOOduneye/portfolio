cbe7abe6dae7fcca23a22570aaa5288b154a32e4
David Oduneye <davidoduneye1@gmail.com>
2024-01-01 16:25:52 -0500

    build: resolved build issues

--- app/(admin)/admin/posts/[postId]/page.tsx
+++ app/(admin)/admin/posts/[postId]/page.tsx
@@ -1,6 +1,5 @@
 import { usePost } from "@/hooks/use-post";
 import React, { useEffect, useRef } from 'react';
-import EditorJS from '@editorjs/editorjs';
 
 interface PostProps {
     params: {
@@ -8,17 +7,13 @@ interface PostProps {
     };
 }
 
-const editor = new EditorJS({
-    holder: 'editorjs', // Replace with the ID of the container div
-    // Other configurations as per your needs
-});
-
 const Post = ({ params }: PostProps) => {
 
     return (
         <div className="container mx-auto">
-            <div id="editorjs"></div>
         </div>
     );
 
 };
+
+export default Post;
