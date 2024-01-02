3e9f303376a99810942809bc475e2798b0369785
David Oduneye <davidoduneye1@gmail.com>
2024-01-02 12:10:31 -0500

    fix: style issues

--- app/(admin)/admin/_components/card-skeleton.tsx
+++ app/(admin)/admin/_components/card-skeleton.tsx
@@ -2,9 +2,9 @@ import { Skeleton } from "@/components/ui/skeleton"
 
 export const CardSkeleton = () => {
     return (
-        <div className="flex flex-row p-5 mb-2 justify-between border border-transparent border-gray-200 shadow-sm dark:border-gray-900 rounded-xl">
+        <div className="flex flex-row p-5 justify-between border border-transparent border-gray-200 shadow-sm dark:border-gray-900">
             <div className="flex flex-col justify-between gap-x-4 w-full">
-                <Skeleton className="w-1/2 h-6 mb-2" />
+                <Skeleton className="w-1/2 h-4 mb-2" />
                 <Skeleton className="w-1/4 h-4" />
             </div>
             <div className="flex gap-2">
