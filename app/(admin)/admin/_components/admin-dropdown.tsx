de9e4ba787f6b04a73a661ce656ba93fbbed948e
David Oduneye <davidoduneye1@gmail.com>
2024-10-13 16:08:39 -0400

    fix: make props optional to create more specificity in the settings of the admin dropdown

--- app/(admin)/admin/_components/admin-dropdown.tsx
+++ app/(admin)/admin/_components/admin-dropdown.tsx
@@ -1,39 +1,49 @@
 import { MoreVertical } from "lucide-react";
 
 import {
-    DropdownMenu,
-    DropdownMenuContent,
-    DropdownMenuItem,
-    DropdownMenuLabel,
-    DropdownMenuSeparator,
-    DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu"
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuLabel,
+  DropdownMenuSeparator,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu";
 import { ConfirmModal } from "@/components/modals/confirm-modal";
 
 interface AdminDropdownProps {
-    onEdit: () => void;
-    onDelete: () => void;
+  onEdit?: () => void;
+  onDelete?: () => void;
 }
 
 export const AdminDropdown = ({ onEdit, onDelete }: AdminDropdownProps) => {
-    return (
+  return (
+    <>
+      {onEdit || onDelete ? (
         <DropdownMenu>
-            <DropdownMenuTrigger className='self-center h-10'>
-                <div className='flex flex-row items-center justify-between p-3 border-2 rounded-md gap-x-4 hover:bg-gray-100 dark:hover:bg-gray-800'>
-                    <MoreVertical className='w-4 h-4' />
-                </div>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align='end'>
-                <DropdownMenuItem>
-                    <DropdownMenuLabel onClick={onEdit}>Edit</DropdownMenuLabel>
-                </DropdownMenuItem>
-                <DropdownMenuSeparator />
-                <DropdownMenuItem>
-                    <ConfirmModal onConfirm={onDelete}>
-                        <DropdownMenuLabel>Delete</DropdownMenuLabel>
-                    </ConfirmModal>
-                </DropdownMenuItem>
-            </DropdownMenuContent>
+          <DropdownMenuTrigger className="self-center h-10">
+            <div className="flex flex-row items-center justify-between p-3 border-2 rounded-md gap-x-4 hover:bg-gray-100 dark:hover:bg-gray-800">
+              <MoreVertical className="w-4 h-4" />
+            </div>
+          </DropdownMenuTrigger>
+          <DropdownMenuContent align="end">
+            {onEdit && (
+              <DropdownMenuItem>
+                <DropdownMenuLabel onClick={onEdit}>Edit</DropdownMenuLabel>
+              </DropdownMenuItem>
+            )}
+
+            {onEdit && onDelete ? <DropdownMenuSeparator /> : null}
+
+            {onDelete && (
+              <DropdownMenuItem>
+                <ConfirmModal onConfirm={onDelete}>
+                  <DropdownMenuLabel>Delete</DropdownMenuLabel>
+                </ConfirmModal>
+              </DropdownMenuItem>
+            )}
+          </DropdownMenuContent>
         </DropdownMenu>
-    )
-}
+      ) : null}
+    </>
+  );
+};
